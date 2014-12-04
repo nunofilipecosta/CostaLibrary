4e0f7de8d86fd9eb997d93abd39ff434ae7814b1
Nuno Costa <nunofilipe.costa@gmail.com>
2014-12-04 22:01:53 +0100

    ADDED : first DateTimeExtensions

---
+++ CostaLibrary.ExtensionMethods.UnitTests/DateTimeExtensionsTests.cs
@@ -0,0 +1,39 @@
+﻿namespace CostaLibrary.ExtensionMethods.UnitTests
+{
+    using System;
+    using System.Xml;
+    using Xunit;
+
+    public class DateTimeExtensionsTests
+    {
+        [Fact]
+        public void ToLelacyFormat_C20()
+        {
+            var date = new DateTime(1920, 12, 31);
+            var result = date.ToLegacyFormat();
+
+            Assert.Equal("0201231", result);
+        }
+
+        [Fact]
+        public void ToLelacyFormat_C21()
+        {
+            var date = new DateTime(2013, 10, 31);
+            var result = date.ToLegacyFormat();
+
+            Assert.Equal("1131031", result);
+        }
+
+        [Fact]
+        public void ToXmlDateTime()
+        {
+            var date = new DateTime(2013, 10, 24, 13, 10, 15, 951);
+            var result = date.ToXmlDateTime();
+            var result2 = date.ToXmlDateTime(XmlDateTimeSerializationMode.Local);
+
+            Assert.Equal("2013-10-24T13:10:15.951Z", result);
+            Assert.Equal("2013-10-24T13:10:15.951+02:00", result2);
+
+        }
+    }
+}
---
+++ src/CostaLibrary.ExtensionMethods/DateTimeExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace CostaLibrary.ExtensionMethods
+{
+    using System;
+    using System.Xml;
+
+    public static class DateTimeExtensions
+    {
+        public static string ToLegacyFormat(this DateTime dateTime)
+        {
+            var formattedDate = dateTime.Year > 1930
+                ? dateTime.ToString("1yyMMdd")
+                : dateTime.ToString("0yyMMdd");
+
+            return formattedDate;
+        }
+
+        public static string ToXmlDateTime(this DateTime dateTime, XmlDateTimeSerializationMode mode = XmlDateTimeSerializationMode.Utc)
+        {
+            var result = XmlConvert.ToString(dateTime, mode);
+
+            return result;
+        }
+    }
+}
