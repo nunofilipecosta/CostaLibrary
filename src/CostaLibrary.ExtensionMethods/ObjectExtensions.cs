2dd7ed2883a7b78028a3948417ef5ec8694c9c5a
Nuno Costa <nunofilipe.costa@gmail.com>
2014-12-05 00:00:30 +0100

    ADDED : ObjectExtensions

---
+++ src/CostaLibrary.ExtensionMethods/ObjectExtensions.cs
@@ -0,0 +1,11 @@
+﻿
+namespace CostaLibrary.ExtensionMethods
+{
+    public static class ObjectExtensions
+    {
+        public static string Get(this object obj)
+        {
+            return obj.GetType().AssemblyQualifiedName;
+        }
+    }
+}
